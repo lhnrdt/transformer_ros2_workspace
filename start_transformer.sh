ec3e289fe22a6f3b17597b3669597d2a671da10a
Johannes <j.lehnerdt@tum.de>
2025-10-17 16:42:23 +0200

    urdf updates

--- start_transformer.sh
+++ start_transformer.sh
@@ -4,6 +4,6 @@ source /home/atvpi/transformer_ros2_workspace/install/setup.bash
 
 exec ros2 launch transformer_bringup transformer_bringup.launch.py \
 	start_realsense:=true \
-	start_wheeltec_imu:=false \
+	start_wheeltec_imu:=true \
 	wheeltec_serial_port:=/dev/ttyUSB0 \
 	wheeltec_serial_baud:=921600
